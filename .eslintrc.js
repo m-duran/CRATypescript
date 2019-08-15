49098e7b02f75de6aed80725ae70d68d8de1501c
Miguel Duran <mduran0314@gmail.com>
2019-08-15 16:11:04 -0700

    add eslint packages and config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
